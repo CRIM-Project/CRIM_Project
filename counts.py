238ac9ca907e35766f423ef0cd940907d624c4a8
Tosin Alliyu <oballiyu@haverford.edu>
2018-02-01 11:19:48 -0500

    updating files

--- counts.py
+++ counts.py
@@ -17,27 +17,27 @@ def get_counts(counts_wanted, data):
 
 	r_keys = [ u'titleA', u'titleB', u'scoreA_ema', u'scoreB_ema', u'direction', u'types']  # ideally not use all of these?
 	#r_keys = [u'scoreA_meiids', u'titleA', u'titleB', u'scoreA_ema', u'scoreB_ema', u'scoreA', u'scoreB', u'cid', u'boolDir', u'direction', u'comment', u'scoreB_meiids', u'scoreAassert', u'scoreBassert', u'id', u'types']  # ideally not use all of these?
-	
+
 	a_keys = [u'ema', u'score', u'title', u'types']
 
 	tempdict = dict(zip(r_keys,[None]*len(r_keys)))
-	
+
 	a_tempdict = dict(zip(a_keys,[None]*len(a_keys)))
 
 
 	for rel in data:
-		
+
 		info = rel["text"]
 		info2 = json.loads(info) # turns 'text' into usable dict
-		
+
 		for i, char in enumerate(info):
 			if char == "r" and info[i-1] == "e" and info[i-2] == "s" and info[i-3] == "u" and info[i+1] == '"':
 				usr = info[i+4]
-				if info[i+5] != '"': 
+				if info[i+5] != '"':
 					usr = usr + info[i+5]
-				if usr not in user_counts: 
+				if usr not in user_counts:
 					user_counts[usr] = 1
-				else: 
+				else:
 					user_counts[usr] += 1
 
 		relation = info2['relationships'][0]
@@ -46,14 +46,14 @@ def get_counts(counts_wanted, data):
 			assertion = info2['assertions'][0]
 		versions = info2['scores']
 		#print(versions)
-		
+
 		for x in versions:
 			title = x['title']
 			#print(x)
-			
-			if title[:2] == ': ': 
+
+			if title[:2] == ': ':
 				title = title[2:] #cleaning some text
-			
+
 			if title in title_counts.keys():
 				temp = title_counts[title]
 				title_counts[title] = temp+1
@@ -67,8 +67,8 @@ def get_counts(counts_wanted, data):
 				#print ("ema_1", relation['scoreA_ema'])
 				#print ("ema_2", relation['scoreA_ema'].split('/')[0])
 				#print ("ema_3", relation['scoreA_ema'].split('/'), "\n")
-		
-				
+
+
 
 		try:
 			direction = relation['direction']
@@ -79,21 +79,21 @@ def get_counts(counts_wanted, data):
 			relationship_counts[direction] = temp+1
 		else: #doesnt exist yet
 			relationship_counts[direction] = 1
-		
+
 		#print( 'here',info2['assertions'])
 		#tempdict = dict(zip(r_keys,[None]*len(r_keys)))
 		#makes data structure like {u'scoreA_meiids': None, u'titleA': None,...}
 
 		for key in r_keys:
 			try:
-				relat = str(relation[key]) 
+				relat = str(relation[key])
 				if key == "types":
 					#print (relation[key])
 					if list(relation[key]) == []:
 						relat = "None"
 					else:
 						relat = list(relation[key])
-								
+
 					#print ("lis")
 					#print (list(relation[key]))
 			except KeyError:
@@ -109,22 +109,22 @@ def get_counts(counts_wanted, data):
 						tempdict[key][elem] = temp1+1
 					else: #doesnt exist yet
 						tempdict[key][elem] = 1
-			
+
 			elif tempdict[key] == None:
-				tempdict[key] = {}	
+				tempdict[key] = {}
 			elif relat in list(tempdict[key]): # hash issue with lists
 				temp1 = tempdict[key][relat]
-				tempdict[key][relat] = temp1+1					
+				tempdict[key][relat] = temp1+1
 			else: #doesnt exist yet
 				tempdict[key][relat] = 1
 
-		
+
 		for key in a_keys:
 			try:
-				assertt = str(assertion[key]) 
-				
+				assertt = str(assertion[key])
+
 				if key == "title":
-					if assertt[:2] == ': ': 
+					if assertt[:2] == ': ':
 						assertt = assertt[2:] #cleaning some text
 				if key == "types":
 					#print (relation[key])
@@ -132,10 +132,10 @@ def get_counts(counts_wanted, data):
 						assertt = "None"
 					else:
 						assertt = list(assertion[key]) # hopefully this works
-					
+
 			except KeyError:
                         	assertt = "None"
-		
+
 			if type(assertt) == type([]):
 				for elem in assertt:
 					if a_tempdict[key] == None:
@@ -145,17 +145,17 @@ def get_counts(counts_wanted, data):
 						a_tempdict[key][elem] = temp1+1
 					else: #doesnt exist yet
 						a_tempdict[key][elem] = 1
-						
+
 			elif a_tempdict[key] == None:
-				a_tempdict[key] = {}	
+				a_tempdict[key] = {}
 			elif assertt in list(a_tempdict[key]): # hash issue with lists
 				temp1 = a_tempdict[key][assertt]
-				a_tempdict[key][assertt] = temp1+1					
+				a_tempdict[key][assertt] = temp1+1
 			else: #doesnt exist yet
 				a_tempdict[key][assertt] = 1
-			
-			
- 
+
+
+
 	#pprint.pprint(tempdict)
 	#pprint.pprint(a_tempdict)
 	#pprint.pprint(ema_dictionary)
@@ -170,7 +170,7 @@ def basic_dict_csv(d,header,filename):
 def to_json(d, filename):
 	with open(filename, 'w') as f:
 		json_str = json.dump(d, f, indent=4)
-		
+
 
 def write_to_csv(count_vals):
 	pass
@@ -180,15 +180,15 @@ def main():
 	crim.set_url(crim.CRIM_url)
 	data = crim.get_data()
 	count_list,title_counts,relationship_counts,tempdict, a_tempdict, ema_dictionary = get_counts(counts_wanted, data)
-	#print ('User: Count')	
+	#print ('User: Count')
 	#pprint.pprint(count_list)
 	#print ("\n")
 	#print ('Song: Count')
 	#pprint.pprint(title_counts)
 	#print ("\n")
-	#print ('Relationship(direction): Count')	
+	#print ('Relationship(direction): Count')
 	#pprint.pprint(relationship_counts,indent=2)
-	
+
 	to_json(ema_dictionary, 'ema.json')
 	#basic_dict_csv(ema_dictionary, 'ema', 'ema.csv')
 	#basic_dict_csv(tempdict['types'],'realationship_types', 'relationship_types.csv')
@@ -198,7 +198,7 @@ def main():
 	#basic_dict_csv(a_tempdict['title'], 'assertion_titles', 'assertion_titles.csv')
 	#basic_dict_csv(a_tempdict['score'], 'assertion_scores', 'assertion_scores.csv')
 
-	
+
 
 if __name__ == "__main__":
 	main()
