e465fa704e57e25bece00f35f3fada4eb1f8db3b
Maddy <mfhodges@haverford.edu>
2018-03-20 09:07:42 -0400

    hmmm

--- heatmap/script.js
+++ heatmap/script.js
@@ -38,7 +38,7 @@ function findArray(value, obJ){
 function genData(){
   //console.log("vv", JSON.stringify(jsonF));
   console.log("got here");
-  console.log("title", title);
+//  console.log("title", title); returns undefined
   var forVis =[];
   var x = 0;
   for(var title in jsonfile){
